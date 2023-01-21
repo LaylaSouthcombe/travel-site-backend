0ace2d836e128975824da9b216762c6592c98200
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-01-21 23:36:39 +0000

    un comment out trending route

--- api/routes/articles.js
+++ api/routes/articles.js
@@ -12,7 +12,7 @@ router.get('/country/:country', articlesController.showCountryArticles);
 router.get('/continent/:continent', articlesController.showContinentArticles);
 router.get('/category/:category', articlesController.showCategoryArticles);
 router.get('/search/:searchterm', articlesController.searchArticles);
-// router.get('/trending', articlesController.showTrendingArticles);
+router.get('/trending', articlesController.showTrendingArticles);
 // router.get('/suggested', articlesController.showSuggestedArticles);
 
 module.exports = router;
